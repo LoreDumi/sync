8f4fa80d51fbb4570a627d2defcf3d10d57c1845
LoreDumi <loredanadumitrescu81@gmail.com>
2024-10-21 10:46:41 +0300

    Add files via upload

---
+++ Crud/main.py
@@ -0,0 +1,76 @@
+import psycopg2
+import pandas as pd
+from flask import Flask, request, jsonify
+from contextlib import closing
+import csv
+import uuid
+
+
+app = Flask(__name__) 
+ 
+def load_data(filename):
+    myList = []
+    with open(filename) as items:
+        data = csv.reader(items, delimiter=',')
+        next(data)
+        for row in data:
+            add_data_in_table(row)
+           # myList.append(row)
+        return myList
+
+def add_data_in_table(row):
+    print('--->>>', row[1])
+    qInsert =""" INSERT INTO  movies ( name, type, description_1, description_2, description_3, description_4, description_5, description_6, description_7, description_8, description_9, description_10, created_year) VALUES
+                   (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s )
+    """
+    cursor.execute( qInsert, (row[1], row[2],row[3], row[3],row[3], row[3], row[3],row[3],row[3],row[3],row[3], row[3],row[4]) )
+
+@app.route("/test")
+def test():
+    open_connection()
+    create_table()
+    url = "https://raw.githubusercontent.com/amirtds/kaggle-netflix-tv-shows-and-movies/main/titles.csv"
+    new_list = load_data('convertcsv.csv')
+    #data = pd.read_csv(url) # use sep="," for coma separation. 
+    #data.describe()
+    #for row in new_list:
+        #print('===>>>', row)
+
+    close_connection()
+    return "test"
+
+def open_connection():
+    #connect to DB and create a table
+    global conn
+    conn =  psycopg2.connect(host="localhost", dbname="electric", user="postgres", password="password", port=54321)
+    global cursor 
+    cursor = conn.cursor()
+
+def close_connection():
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+def create_table():
+    cursor.execute(""" CREATE TABLE IF NOT EXISTS movies (
+               id SERIAL PRIMARY KEY,
+               name VARCHAR,
+               type VARCHAR,
+               description_1 VARCHAR,
+               description_2 VARCHAR,
+               description_3 VARCHAR,
+               description_4 VARCHAR,
+               description_5 VARCHAR,
+               description_6 VARCHAR,
+               description_7 VARCHAR,
+               description_8 VARCHAR,
+               description_9 VARCHAR,
+               description_10 VARCHAR,
+               created_year VARCHAR
+               );
+    """)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
